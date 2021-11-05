37d8cd20911cc2f4ad2e3ebb03d2306a25ab6a47
AbuzarDar <88462584+AbuzarDar@users.noreply.github.com>
2021-11-05 17:01:49 +0500

    Band website files

---
+++ index.js
@@ -0,0 +1,53 @@
+
+//DETECTING MOUSE CLICKED ON BUTTON W A S D J K OR L
+
+var totalDrums=document.querySelectorAll(".drum").length;
+for(var i=0;i<totalDrums;i++){
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    var buttonInnerHtml=this.innerHTML;
+    makeSound(buttonInnerHtml);
+  });
+}
+
+//DETECTING KEYBOARD BUTTON PRESS
+
+document.addEventListener("keydown",function(event){
+  makeSound(event.key);
+});
+
+//FUNTION OF PRODUCING SOUND ON KEYPRESS OR CLICKED
+
+function makeSound(key){
+  switch(key){
+    case "w":
+      var tom4=new Audio('sounds/tom-4.mp3');
+      tom4.play();
+      break;
+    case "a":
+      var tom1=new Audio('sounds/tom-1.mp3');
+      tom1.play();
+      break;
+    case "s":
+      var tom2=new Audio('sounds/tom-2.mp3');
+      tom2.play();
+      break;
+    case "d":
+      var tom3=new Audio('sounds/tom-3.mp3');
+      tom3.play();
+      break;
+    case "j":
+      var snare=new Audio('sounds/snare.mp3');
+      snare.play();
+      break;
+    case "k":
+      var kick=new Audio('sounds/kick-bass.mp3');
+      kick.play();
+      break;
+    case "l":
+      var crash=new Audio('sounds/crash.mp3');
+      crash.play();
+      break;
+    default:
+      alert("Press The Correct Button: Currently You Pressed"+ buttonInnerHtml);
+  }
+}
